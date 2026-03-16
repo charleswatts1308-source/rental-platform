e496a954ea3a2b6f0d837c02f30b20eae8399c4d
charleswatts1308-source <charles.watts31308@gmail.com>
2026-03-16 09:47:59 +0000

    Update welcome page content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,27 +8,33 @@
 <p>If you live in the UK and rent your home<br>
    This is for you.<br>
 <br>
+The new Renters Rights Act is coming into force on May 1st.<br>
+In just {{ (int) \Carbon\Carbon::now()->diffInDays(\Carbon\Carbon::create(2026, 5, 1), false) }} days
 
-A powerful change in our UK rental Law is coming on May 1st.<br>
 <br>
-The change brings protection from unfair eviction<br>
+This brings many changes but one powerful change stands out.<br>
+<br>
+You will be protected by law from unfair eviction<br>
 It frees you to safely push landlords for maintenance and repairs and a better deal.<br>
 <br>
-
-Welcome to our Renters letter writing service
-Think letter 1, etc until they give in or give up
-<br><br>
-Sign up to our Landlord Contact Service<br>
-and improve the condition of the UK PRS
+This is the biggest change for UK renters in 100 years.<br>
+The removal of Section21 combined with the power of internet communities means you can shake the UK rental market to its core. <br>
+<br>
+Think I’m exaggerating ?  <br>
+Sign up and find out for yourself.<br>
+<br>
+Sign-up is free and once registered you can see our site content.
 <br>
 
-
-
 <div class="alert alert-success mb-4">
-    <p class="lead mb-0"><strong>Join us - sign up and be counted</strong></p>
+    <p class="lead mb-0"><strong>Join us</strong></p>
 </div>
 
 <div class="text-center mt-4">
     <a href="{{ route('register') }}" class="btn btn-primary btn-lg px-5">Register</a>
 </div>
 @endsection
+
+
+{{-- And to celebrate this huge change I bring you this platform to help you
+pursue better conditions and better terms. --}}
